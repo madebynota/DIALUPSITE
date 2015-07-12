b03ff3686d41a337b5a6384575401fff7d2d98ff
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2015-07-12 12:30:15 -0400

    Added basic support for Bespoke Magazine viewer

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {res.render('index.html')});
 app.get('/portal', function(req, res) {res.render('portal.html')});
+app.get('/magazine', function(req, res) {res.render('magazine.html')});
+app.get('/summer2015', function(req, res) {res.render('summer2015.html')});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
