63a174d5b6e98e9c549c68f2c005519717866015
root <root@droplet.dialupstuff.com>
2016-05-19 00:19:35 -0400

    Fixed server issues

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 80));
 app.set('views', __dirname + '/public');
 app.set('view engine', 'html');
 app.engine('html', engines.mustache);
@@ -44,3 +44,4 @@ app.use(function(req, res, next) {
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
+
