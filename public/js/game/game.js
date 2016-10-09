307aef5cc22e3f2d181f52da12778a969dbc568a
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-10-09 15:31:23 -0700

    Added some basic imagery for ground and sky

--- public/js/game/game.js
+++ public/js/game/game.js
@@ -1,5 +1,6 @@
 var game = new Phaser.Game(500, 700, Phaser.AUTO, 'game');
 var game_state = {};
+var platforms;
 
 // Creates a new 'main' state that wil contain the game
 game_state.main = function() { };  
@@ -7,10 +8,33 @@ game_state.main.prototype = {
 
     preload: function() { 
 		// Function called first to load all the assets
-        game.load.image('sprite', 'img/bo2.png');
+        game.load.image('sky', 'img/game/sky.jpg');
+        game.load.image('ground', 'img/game/ground.png');
+        game.load.image('sprite', 'img/bo2.png', 32, 48);
     },
 
     create: function() { 
+         //  We're going to be using physics, so enable the Arcade Physics system
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //  A simple background for our game
+        game.add.sprite(0, 0, 'sky');
+
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = game.add.group();
+
+        //  We will enable physics for any object that is created in this group
+        platforms.enableBody = true;
+
+        // Here we create the ground.
+        var ground = platforms.create(-10, game.world.height - 64, 'ground');
+
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground.scale.setTo(2, 2);
+
+        //  This stops it from falling away when you jump on it
+        ground.body.immovable = true;
+
     	// Fuction called after 'preload' to setup the game
         this.sprite = game.add.sprite(30, 30, 'sprite');
     },
