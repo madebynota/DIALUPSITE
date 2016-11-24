85c048d6761f588cdfafd148f1f475348927b0bc
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-11-24 15:46:32 -0800

    Drops isomorphic react (server-side react) and allows client-side to handle all routing and rendering

--- src/server.js
+++ src/server.js
@@ -1,60 +1,25 @@
-import path from 'path';
-import { Server } from 'http';
-import Express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-import routes from './routes';
-import NotFoundPage from './components/NotFoundPage';
+// import Express from 'express';
+var path = require('path');
+var Express = require('express');
+var engines = require('consolidate');
+var mustache = require('mustache');
 
-// initialize the server and configure support for ejs templates
 const app = new Express();
-const server = new Server(app);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-// define the folder that will be used for static assets
-app.use(Express.static(path.join(__dirname, 'static')));
+app.set('port', (process.env.PORT || 3000));
+app.set('env', (process.env.NODE_ENV || 'production'));
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'static'));
+app.engine('html', engines.mustache);
+app.use(Express.static(__dirname + '/static'))
 
-// universal routing and rendering
-app.get('*', (req, res) => {
-    match(
-        { routes, location: req.url },
-        (err, redirectLocation, renderProps) => {
-
-        // in case of error display the error message
-        if (err) {
-            return res.status(500).send(err.message);
-        }
-
-        // in case of redirect propagate the redirect to the browser
-        if (redirectLocation) {
-            return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-        }
-
-        // generate the React markup for the current route
-        let markup;
-        if (renderProps) {
-            // if the current route matched we have renderProps
-            markup = renderToString(<RouterContext {...renderProps}/>);
-        } else {
-            // otherwise we can render a 404 page
-            markup = renderToString(<NotFoundPage/>);
-            res.status(404);
-        }
-
-        // render the index template with the embedded React markup
-        return res.render('index', { markup });
-        }
-    );
+// Universal routing and rendering handled by React & react-router
+// on the client-side.
+app.get('*', function(req, res) {
+    res.render('index.html');
 });
 
 // start the server
-const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'production';
-server.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
-    console.info(`Server running on http://localhost:${port} [${env}]`);
+app.listen(app.get('port'), function(){
+    console.log('[' + app.get('env') + '] Express server listening on port ' + app.get('port'));
 });
