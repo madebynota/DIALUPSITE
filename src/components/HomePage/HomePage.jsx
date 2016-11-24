690daad5e800f2ad2f0ab098833ac131e034f000
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-11-24 03:37:14 -0800

    Moved Hompage to folder, and started component for Non-Safari home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import NonSafariHomePage from './NonSafariHomePage';
 
 class HomePage extends React.Component {
     render() {
         return (
             <div>
-                <h1>HOME PAGE</h1>
+                <NonSafariHomePage />
             </div>
         );
     }
---
+++ src/components/HomePage/NonSafariHomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+import LogoSection from './LogoSection';
+
+class NonSafariHomePage extends React.Component {
+    render() {
+        return (
+            <div>
+				<h1>Non Safari Home Page</h1>
+            </div>
+        );
+    }
+}
+
+export default NonSafariHomePage;
