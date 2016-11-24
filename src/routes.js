c74e1e07278ccf76e90532779a45e329de4c3634
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-11-24 00:22:00 -0800

    Adds Node.js server with NotFoundPage component and updated start command

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App';
 import HomePage from './components/HomePage';
 import MagazinesPage from './components/MagazinesPage';
+import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
 	<Route path="/" component={App}>
 		<IndexRoute component={HomePage} />
 		<Route path="magazines" component={MagazinesPage} />
+		<Route path="*" component={NotFoundPage} />
 	</Route>
 );
 
