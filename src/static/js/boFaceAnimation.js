b9afe36827909a4f5be4c198c240e373e9bed2b3
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-11-25 08:16:50 -0800

    Refactors boFace file to use class structure to minimize repeated code and optimize click handling

--- src/static/js/boFaceAnimation.js
+++ src/static/js/boFaceAnimation.js
@@ -1,76 +1,95 @@
 var width, height;
 var xpos, ypos;
 var yvel, xvel;
-var img, img2;
+var face1, face2;
 var imgWidth, imgHeight;
-var link;
-var gifStart;
 
 function setup() {
 
     width = window.innerWidth;
     height = window.innerHeight;
+    createCanvas(width,height);
 
     if(width > 768){
-        imgWidth = width/7;
-        imgHeight = width/10;
+        imgWidth = width / 7;
+        imgHeight = width / 10;
     } else {
-        imgWidth = width/3;
-        imgHeight = width/5;
+        imgWidth = width / 3;
+        imgHeight = width / 5;
     }
 
-    xpos = floor(random(imgWidth, width-imgWidth));
-    ypos = floor(random(imgHeight, height-imgHeight));
+    xpos = floor(random(imgWidth, width - imgWidth));
+    ypos = floor(random(imgHeight, height - imgHeight));
 
     xvel = random(-4, 4);
     yvel = random(-4, 4);
 
-    createCanvas(width,height);
+    //Order matters, since we want face 1 to be on top
+    face2 = new BoFace(
+        "bo2", 
+        "/img/boFace/bo2.png", 
+        imgWidth, 
+        imgHeight,
+        xpos,
+        ypos,
+        xvel,
+        yvel,
+        false
+    );
 
-    gifStart = false;
-
-    img2 = createImg("/img/boFace/bo2.png");
-    img = createImg("/img/boFace/bo.png");
-
-    img2.position(xpos, ypos);
-    img2.size(imgWidth, imgHeight);
-    img2.id("bo2");
-
-    img.position(xpos, ypos);
-    img.size(imgWidth, imgHeight);
-    img.id("bo");
-
-    img.mousePressed(stop);
-    img2.mousePressed(stop);
+    face1 = new BoFace(
+        "bo", 
+        "/img/boFace/bo.png", 
+        imgWidth, 
+        imgHeight,
+        xpos,
+        ypos,
+        xvel,
+        yvel,
+        true
+    );
 }
 
 function draw() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-
-    if((xpos >= (width-(imgWidth))) | (xpos <= 0)){
-        xvel *= -1;
-    }
-
-    if((ypos >= (height-(imgHeight))) | (ypos <= 0)){
-        yvel *= -1;
-    }
-
-    xpos += xvel;
-    ypos += yvel;
-
-    img2.position(xpos, ypos);
-    img.position(xpos, ypos);
+    face1.move();
+    face2.move();
 }
 
-function stop() {
-    img.hide();
+class BoFace {
+    constructor(id, imgPath, width, height, xPos, yPos, xVel, yVel, shouldHide) {
+        this.id = id;
+        this.width = width;
+        this.height = height;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xVel = xVel;
+        this.yVel = yVel;
+        this.shouldHide = shouldHide;
 
-    switchBackground(window.innerWidth);
-    
-    if(width<767){
-        switchBackgroundMobile();
-    } else {
-        switchBackground();
+        this.img = createImg(imgPath);
+        this.img.position(this.xPos, this.yPos);
+        this.img.size(this.width, this.height);
+        this.img.id(this.id);
+        this.img.mousePressed(this.click.bind(this));
+        console.log(this.img);
+    }
+    move() {
+        if ((this.xPos >= (window.innerWidth - (this.width))) | (this.xPos <= 0)) {
+            this.xVel *= -1;
+        }
+
+        if ((this.yPos >= (window.innerHeight - (this.height))) | (this.yPos <= 0)) {
+            this.yVel *= -1;
+        }
+
+        this.xPos += this.xVel;
+        this.yPos += this.yVel;
+        this.img.position(this.xPos, this.yPos); 
+    }
+    click() {
+        if (this.shouldHide) {
+            this.img.hide();
+        }
+        switchBackground(window.innerWidth);
     }
 }
