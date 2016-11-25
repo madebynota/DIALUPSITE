c1447951e0917e03242b07e04f32381bad9cf3df
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-11-24 18:59:24 -0800

    Adds link items section to home page

---
+++ src/components/HomePage/LinksSection.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router';
+import classNames from 'classnames/bind';
+import styles from './styles/Link.css';
+
+let cx = classNames.bind(styles);
+
+const linkItems = [
+    {
+        text: "MAGAZINE",
+        path: "/magazines"
+    },
+    {
+        text: "MUSIC",
+        path: "https://soundcloud.com/dialupstuff"
+    },
+    {
+        text: "VIDEO",
+        path: "https://www.youtube.com/channel/UCLbmvnHClDh_y8imC1wAuDA"
+    },
+    {
+        text: "PODCAST",
+        path: "https://soundcloud.com/dial-up-podcast"
+    },
+    {
+        text: "PHOTO",
+        path: "https://instagram.com/dialupstuff"
+    },
+    {
+        text: "CONTACT",
+        path: ""
+    }
+];
+
+class LinkItem extends React.Component {
+    openContactAlert() {
+        let contactString = "EMAIL US AT DIALUPSTUFF@GMAIL.COM\n\nFACEBOOK AT HTTPS://WWW.FACEBOOK.COM/DIALUPSTUFF";
+        alert(contactString);
+    }
+    render() {
+        let target = this.props.text.toLowerCase().replace(/\s/g,'');
+        return this.props.text == "CONTACT"
+            ?(
+                <div className={cx('linkItem')}>
+                    <a style={{"color": this.props.color}} onClick={this.openContactAlert}>
+                        <p>{this.props.text}</p>
+                    </a>
+                </div>
+            )   
+            :(
+                <div className={cx('linkItem')}>
+                    <a style={{"color": this.props.color}} target={target} href={this.props.path}>
+                        <p>{this.props.text}</p>
+                    </a>
+                </div>
+            )
+    }
+}
+
+class LinksSection extends React.Component {
+    generateLinkItems(items, color) {
+        let links = [];
+        for (let item of items) {
+          links.push(<LinkItem key={item.text} text={item.text} path={item.path} color={color}/>);
+        }
+        return links;  
+    }
+    render() {
+        return (
+            <div>
+                {this.generateLinkItems(linkItems, "#000000")}
+            </div>
+        );
+    }
+}
+
+export default LinksSection;
--- src/components/HomePage/NonSafariHomePage.jsx
+++ src/components/HomePage/NonSafariHomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 import classNames from 'classnames/bind';
 import LogoSection from './LogoSection';
+import LinksSection from './LinksSection';
 import styles from './styles/nonsafari.css';
 
 let cx = classNames.bind(styles);
@@ -13,6 +14,11 @@ class NonSafariHomePage extends React.Component {
 				<div className={'row'}>
 					<div className={classNames('col-md-6', 'col-md-offset-3')}>
 						<LogoSection styles={cx('logo')} />
+					</div>
+				</div>
+				<div className={'row'}>
+					<div className={classNames('col-md-6', 'col-md-offset-3')}>
+						<LinksSection />
 					</div>
 				</div>
             </div>
