99e4d0e7760e8174ad8729293b678199a4ae1683
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-11-25 19:35:32 -0800

    Renames NonSafari component and CSS to home page. We don't need separate components for each browser.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router';
-import NonSafariHomePage from './NonSafariHomePage';
+import classNames from 'classnames/bind';
+import VideoBackground from './VideoBackground';
+import LogoSection from './LogoSection';
+import LinksSection from './LinksSection';
+import styles from './styles/NonSafari.css';
 
-class HomePage extends React.Component {
+let cx = classNames.bind(styles);
+
+class NonSafariHomePage extends React.Component {
+	constructor(props) {
+        super(props);
+        this.state = {
+            linkColor: "#000000",
+        };
+    }
+    getLinkColor() {
+    	return this.state.linkColor;
+    }
+    setLinkColor(color) {
+    	this.setState({
+    		linkColor: color
+    	});
+    }
     render() {
         return (
-            <div>
-                <NonSafariHomePage />
+        	<div>
+        		<VideoBackground setLinkColor={this.setLinkColor.bind(this)} />
+	            <div className={classNames('container-fluid', cx('verticalCenter'))}>
+					<div className={'row'}>
+						<div className={classNames('col-md-6', 'col-md-offset-3')}>
+							<LogoSection styles={cx('logo')} />
+						</div>
+					</div>
+					<div className={'row'}>
+						<div className={classNames('col-md-6', 'col-md-offset-3')}>
+							<LinksSection color={this.state.linkColor} />
+						</div>
+					</div>
+	            </div>
             </div>
         );
     }
 }
 
-export default HomePage;
+export default NonSafariHomePage;
