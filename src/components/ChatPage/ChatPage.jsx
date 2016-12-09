0118fee4f41a47b804181b623956d62abb8a3b43
yawkity <sehmon3@gmail.com>
2016-12-08 21:15:52 -0500

    Added base Chatroom page and route

---
+++ src/components/ChatPage/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/ChatPage.css'
+
+class ChatPage extends React.Component {
+  render() {
+    return (
+      <h1>Chatroom Page</h1>
+    )
+  }
+}
+
+export default ChatPage;
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App';
 import HomePage from './components/HomePage/HomePage';
 import MagazinesPage from './components/MagazinesPage';
+import ChatPage from './components/ChatPage/ChatPage'
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
 	<Route path="/" component={App}>
 		<IndexRoute component={HomePage} />
 		<Route path="magazines" component={MagazinesPage} />
+		<Route path="chat" component={ChatPage} />
 		<Route path="*" component={NotFoundPage} />
 	</Route>
 );
