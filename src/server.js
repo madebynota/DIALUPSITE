dfcb472c6c0e2c34cc35d501a6ad7206673161ce
yawkity <sehmon3@gmail.com>
2016-12-08 22:28:19 -0500

    Added socket.io funcitonality and basic client/server events

--- src/server.js
+++ src/server.js
@@ -13,6 +13,16 @@ app.set('views', path.join(__dirname, 'static'));
 app.engine('html', engines.mustache);
 app.use(Express.static(__dirname + '/static'))
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.emit('server event', { foo: 'bar' });
+  socket.on('client event', function (data) {
+    console.log(data);
+  });
+});
+
 // Universal routing and rendering handled by React & react-router
 // on the client-side.
 app.get('*', function(req, res) {
@@ -20,6 +30,11 @@ app.get('*', function(req, res) {
 });
 
 // start the server
-app.listen(app.get('port'), function(){
-    console.log('[' + app.get('env') + '] Express server listening on port ' + app.get('port'));
+// app.listen(app.get('port'), function(){
+//    console.log('[' + app.get('env') + '] Express server listening on port ' + app.get('port'));
+//});
+
+// start the server
+server.listen(app.get('port'), function(){
+   console.log('[' + app.get('env') + '] Express server listening on port ' + app.get('port'));
 });
