869f34c9b73c410a2c34e21d0e732c7913b6a002
yawkity <sehmon3@gmail.com>
2016-12-21 00:18:08 -0500

    Added user labels to chatroom input

--- src/components/ChatPage/MessageForm.jsx
+++ src/components/ChatPage/MessageForm.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 class MessageForm extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {text: ''};
+        this.state = {
+            text: ''
+        };
         this.changeHandler = this.changeHandler.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -12,29 +14,28 @@ class MessageForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         var message = {
-            user : this.props.user,
-            text : this.state.text
+            user: this.props.user,
+            text: this.state.text
         }
         this.props.onMessageSubmit(message);
-        this.setState({ text: '' });
+        this.setState({text: ''});
     }
 
     changeHandler(e) {
-        this.setState({ text : e.target.value });
+        this.setState({text: e.target.value});
     }
 
     render() {
-        return(
-          <div>
-              <h3>Write New Message</h3>
-              <form onSubmit={this.handleSubmit}>
-                  <input
-                      onChange={this.changeHandler}
-                      value={this.state.text}
-                  />
-              </form>
-          </div>
-      );
+        return (
+            <div>
+                <h3>Write New Message</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label>{this.props.user}
+                        <input onChange={this.changeHandler} value={this.state.text}/>
+                    </label>
+                </form>
+            </div>
+        );
     }
 }
 
