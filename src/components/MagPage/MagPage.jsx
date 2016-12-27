ddf2fac70573b848ffc5219a495db1a73e564d5b
David Latimore II <dgl@u.northwestern.edu>
2016-12-27 14:26:29 -0600

    fixed all the issues raised in Bo's PR Review

--- src/components/MagPage/MagPage.jsx
+++ src/components/MagPage/MagPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; // gimme React
 import classNames from 'classnames/bind'; // gotta name more than one class youngblood
-import PreviewRow from './PreviewRow'; // Imports the Mag Previews component
-import styles from './styles/magazinePage.css'; // show me my stylesheets
+import MagIcons from './MagIcons'; // Imports the Mag Previews component
+import styles from './styles/MagPage.css'; // show me my stylesheets
 
 let cx = classNames.bind(styles);
 
@@ -13,28 +13,28 @@ class MagPage extends React.Component {
             	<img className={cx("bkgImage")} src="/img/misc/bedroom.png" />
             	<div className = {cx("headerText")}>
 	            	<h1> MAGAZINES </h1>
-	            	<h4> READ THE NEWEST SUMMER 16 MAG! <br/><br/><br/></h4>
+	            	<h4> READ THE NEWEST SUMMER 16 MAG!</h4>
 	            </div>
-            	<div className={classNames("container-fluid")}>
-            		<div className = "row">
+            	<div className={"container-fluid"}>
 
-            			<div className = {classNames("col-md-3", cx("magPanel"))}>
-		            		<PreviewRow picPath = "/img/magIcons/summer16.png"/>
+            			<div className = {classNames("col-md-6", cx("magPanel"))}>
+		            		<MagIcons picPath = "/img/magIcons/summer16.png"/>
 		            	</div>
 
-            			<div className = {classNames("col-md-3", cx("magPanel"))}>
-		            		<PreviewRow picPath = "/img/magIcons/fall15.png"/>
+            			<div className = {classNames("col-md-6", cx("magPanel"))}>
+		            		<MagIcons picPath = "/img/magIcons/fall15.png"/>
+		            	</div>
+                    </div>
+
+                    <div className = "row">
+                        <div className = {classNames("col-md-6", cx("magPanel"))}>		            		
+		            		<MagIcons picPath = "/img/magIcons/winter16.png"/>
 		            	</div>
 
-		            	<div className = {classNames("col-md-3", cx("magPanel"))}>		            		
-		            		<PreviewRow picPath = "/img/magIcons/winter16.png"/>
-		            	</div>
-
-		            	<div className = {classNames("col-md-3", cx("magPanel"))}>
-		            		<PreviewRow picPath = "/img/magIcons/summer15.png"/>
+		            	<div className = {classNames("col-md-6", cx("magPanel"))}>
+		            		<MagIcons picPath = "/img/magIcons/summer15.png"/>
 		            	</div>
             		</div>
-            	</div>
             </div>
         );
     }
