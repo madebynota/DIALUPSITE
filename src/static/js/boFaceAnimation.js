8a56b94e5aa16233a865b73667af97de5dae463b
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-12-29 13:28:45 -0600

    Adds event listener to the boface image in an attempt to prevent double-tap to zoon

--- src/static/js/boFaceAnimation.js
+++ src/static/js/boFaceAnimation.js
@@ -40,6 +40,9 @@ function setupBoFace() {
         yvel,
         true
     );
+
+    var boFaceElement = document.getElementById("bo");
+    boFaceElement.addEventListener('touchstart', preventZoom);
 }
 
 function drawBoFace() {
@@ -96,4 +99,17 @@ class BoFace {
 
       transitionToNextVideo();
     }
+}
+
+function preventZoom(e) {
+    var t2 = e.timeStamp;
+    var t1 = e.currentTarget.dataset.lastTouch || t2;
+    var dt = t2 - t1;
+    var fingers = e.touches.length;
+    e.currentTarget.dataset.lastTouch = t2;
+
+    if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+    e.preventDefault();
+    e.target.click();
 }
