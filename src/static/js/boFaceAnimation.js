13ae114a591d816362bea19759e6cf3790033265
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-12-29 14:01:25 -0600

    Adds prevent zoom function into BoFace class

--- src/static/js/boFaceAnimation.js
+++ src/static/js/boFaceAnimation.js
@@ -40,9 +40,6 @@ function setupBoFace() {
         yvel,
         true
     );
-
-    var boFaceElement = document.getElementById("bo");
-    boFaceElement.addEventListener('touchstart', preventZoom);
 }
 
 function drawBoFace() {
@@ -65,6 +62,11 @@ class BoFace {
         this.img.size(this.width, this.height);
         this.img.id(this.id);
         this.img.mousePressed(this.click.bind(this));
+        this.img.touchStarted(this.preventZoom);
+
+        // var boFaceElement = document.getElementById(id);
+        
+        // boFaceElement.addEventListener('touchstart', preventZoom);
     }
     changeImage(imgSrc){
       // Clear original image from Canvas
@@ -76,6 +78,7 @@ class BoFace {
       this.img.size(this.width, this.height);
       this.img.id(this.id);
       this.img.mousePressed(this.click.bind(this));
+      this.img.touchStarted(this.preventZoom);
 
       this.shouldSwap = false;
     }
@@ -99,17 +102,18 @@ class BoFace {
 
       transitionToNextVideo();
     }
-}
+    preventZoom(e) {
+        if(e.type !== 'touchstart') return; //not a touch-event 
+        
+        var t2 = e.timeStamp;
+        var t1 = e.currentTarget.dataset.lastTouch || t2;
+        var dt = t2 - t1;
+        var fingers = e.touches.length;
+        e.currentTarget.dataset.lastTouch = t2;
 
-function preventZoom(e) {
-    var t2 = e.timeStamp;
-    var t1 = e.currentTarget.dataset.lastTouch || t2;
-    var dt = t2 - t1;
-    var fingers = e.touches.length;
-    e.currentTarget.dataset.lastTouch = t2;
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
 
-    if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-
-    e.preventDefault();
-    e.target.click();
+        e.preventDefault();
+        e.target.click();
+    }
 }
