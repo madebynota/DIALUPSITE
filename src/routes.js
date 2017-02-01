4f28ad1663eb0d53018aafe22ada4d7f75efc78f
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-01-31 19:42:58 -0600

    Fixes bug in routes.js caused by changing component

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './components/App';
 import HomePage from './components/HomePage/HomePage';
-import ChatPage from './components/ChatPage/ChatPage'
+import RadioPage from './components/RadioPage/RadioPage'
 import MagPage from './components/MagPage/MagPage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
 	<Route path="/" component={App}>
 		<IndexRoute component={HomePage} />
-		<Route path="radio" component={ChatPage} />
+		<Route path="radio" component={RadioPage} />
 		<Route path="magazines" component={MagPage} />
 		<Route path="*" component={NotFoundPage} />
 	</Route>
