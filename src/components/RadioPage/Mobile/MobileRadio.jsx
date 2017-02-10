9d735d07f4388c41b2f0759db675f6f54546f8fa
yawkity <sehmon3@gmail.com>
2017-02-09 22:22:32 -0600

    When device width is less than 760, loads mobile radio component

---
+++ src/components/RadioPage/Mobile/MobileRadio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MobileRadio extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>Yooooooooo</h1>
+        )
+    }
+}
+
+export default MobileRadio
