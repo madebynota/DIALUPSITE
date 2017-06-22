5f60cba84a1f3d7ff6d650e5da1c0bb921894b11
David Latimore <dgl@u.northwestern.edu>
2017-06-21 17:39:10 -0700

    began to stub out DUFC in React

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App';
 import HomePage from './components/HomePage/HomePage';
 import RadioPage from './components/RadioPage/RadioPage';
+import FinancePage from './components/FinancePage/FinancePage';
 import MagPage from './components/MagPage/MagPage';
 import NotFoundPage from './components/NotFoundPage';
 
@@ -10,6 +11,7 @@ const routes = (
 	<Route path="/" component={App}>
 		<IndexRoute component={HomePage} />
 		<Route path="radio" component={RadioPage} />
+		<Route path="finance" component={FinancePage} />
 		<Route path="magazines" component={MagPage} />
 		<Route path="*" component={NotFoundPage} />
 	</Route>
