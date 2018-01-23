254be5721654e01ebe53f758b056886404dffba8
David Latimore <dgl@u.northwestern.edu>
2018-01-23 17:41:02 -0600

    removed weird positioning rule for mobile devices, removed timer function since the countdown is gone

--- src/components/SplashPage/SplashPage.jsx
+++ src/components/SplashPage/SplashPage.jsx
@@ -11,54 +11,6 @@ class SplashPage extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-
-    let canvas = document.getElementById("defaultCanvas0");
-    canvas.parentNode.removeChild(canvas);
-
-
-
-    var countDownDate = new Date("Dec 28, 2017 19:00:00").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-      // Get todays date and time
-      var now = new Date().getTime();
-
-      // Find the distance between now an the count down date
-      var distance = countDownDate - now;
-
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      if (parseInt(hours) < 10) {
-        hours = "0" + hours;
-      }
-
-      if (parseInt(minutes) < 10) {
-        minutes = "0" + minutes;
-      }
-
-      if (parseInt(seconds) < 10) {
-        seconds = "0" + seconds;
-      }
-
-      // Display the result in the element with id="demo"
-      document.getElementById("timer").innerHTML = "0" + days + ":" + hours + ":"
-      + minutes + ":" + seconds;
-      
-      // If the count down is finished, write some text 
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-      }
-    }, 1000);
-  }
-
   render() {
     return (
       <div className={cx("container")}>
