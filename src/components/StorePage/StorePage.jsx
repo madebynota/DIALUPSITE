4b5e693345629858d7d47bb5efe0904fa4dc7750
sehmon <sehmon3@gmail.com>
2018-02-06 22:18:35 -0600

    Added clean store page

--- src/components/StorePage/StorePage.jsx
+++ src/components/StorePage/StorePage.jsx
@@ -18,42 +18,7 @@ class StorePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [
-        {
-          images: [
-            {
-              src: 'www.google.com'
-            }
-          ],
-          variants: [
-            {
-              price: "Free.99"
-            }
-          ],
-          description: 'SuperLucha Tee (White)',
-          price: '$19.99',
-          url: '/store/item/tee',
-          id: 'tee',
-          title: 'Water Bottle'
-        },
-        {
-          images: [
-            {
-              src: 'www.google.com'
-            }
-          ],
-          variants: [
-            {
-              price: "Free.98"
-            }
-          ],
-          description: 'Sticker Pack',
-          price: '$19.99',
-          url: '/store/item/tee',
-          id: 'tee',
-          title: 'Water Bottle'
-        },
-      ]
+      products: []
     }
   }
 
@@ -71,29 +36,7 @@ class StorePage extends React.Component {
 
   render() {
     return (
-      <div className={cx("StorePage")}>
-        <Grid fluid>
-          <Row>
-            <Col xsOffset={3} xs={6} smOffset={5} sm={2}>
-              <img className={cx("header")} src="img/logos/logo-2.png"/>
-            </Col>
-          </Row>
-          <Row className={cx("productContainer")}>
-            {this.state.products.map((product, index) => (
-              <Col xs={12} sm={4} key={index}>
-                <Link to={product.title} >
-                  <ItemCard
-                    image={product.images ? product.images[0].src : 'http://dialupstuff.com/img/logos/logo-2.png'}
-                    description={product.description}
-                    price={product.variants[0].price}
-                  />
-                  </Link>
-              </Col>
-            ))}
-          </Row>
-        </Grid>
-        <p className={cx("storeFooter")}>STORE</p>
-      </div>
+      <h1>Store Page</h1>
     )
   }
 }
