e45e309d8ad9958d0295e43ba2ac3fad55468605
David Latimore II <david.latimore@me.com>
2019-11-07 02:36:44 -0500

    adding hidden/indirect route to production

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import MagPage from './components/MagPage/MagPage';
 import RadioPage from './components/RadioPage/RadioPage';
 import BlogPage from './components/BlogPage/BlogPage';
 import SplashPage from './components/SplashPage/SplashPage';
+import MusicVideoSplash from './components/MusicVideoSplash/SplashPage';
 import PressPage from './components/PressPage/PressPage';
 import NotFoundPage from './components/NotFoundPage';
 
@@ -13,11 +14,12 @@ import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
 	<Route path="/" component={App}>
-		<IndexRoute component={SplashPage} />
+		<IndexRoute component={MusicVideoSplash} />
 		<Route path="home" component={HomePage} />
 		<Route path="magazines" component={MagPage} />
 		<Route path="radio" component={RadioPage} />
 		<Route path="blog" component={BlogPage} />
+		<Route path="eyes" component={SplashPage} />
 		<Route path="*" component={NotFoundPage} />
 	</Route>
 );
