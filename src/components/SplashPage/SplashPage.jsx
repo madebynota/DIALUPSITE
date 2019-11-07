052fdea98d5622fcaed02f77328ce7173e73ffac
David Latimore II <david.latimore@me.com>
2019-11-07 01:04:24 -0500

    initial structure of rsvp form added

--- src/components/SplashPage/SplashPage.jsx
+++ src/components/SplashPage/SplashPage.jsx
@@ -13,24 +13,37 @@ class SplashPage extends React.Component {
 
   render() {
     return (
-      <Grid fluid className={cx('SplashPage')}>
-				<Row className={cx('SplashPage-body')}>
-					<Col xs={12} className={cx('video-container')}>
-            <iframe
-							className={cx("video")}
-							width="840"
-							height="472"
-							src="https://www.youtube.com/embed/QIpf4C480E0"
-							frameborder="0"
-							allow="autoplay; encrypted-media"
-							allowfullscreen>
-						</iframe>
-						<div>
-		          <a className={cx("enter")} href="/home"> ENTER THE SITE </a>
+      <div className={cx('RSVPPage')}>
+        <img src='/img/graphics/title.svg' className={cx('title')}/>
+				<div className={cx('subtitles')}>
+					<h2 className={cx('subtitle')}> release party & live performance </h2>
+					<h2 className={cx('subtitle', 'bottom')}> release party & live performance </h2>
+				</div>
+				<div className={cx('third-row')}>
+					<h3 className={cx('left')} > november 15th, 2019 <br /> 8 P.M. - 11 P.M. </h3>
+					<h3 className={cx('right')} > 1259 n. milwaukee st. <br /> chicago, il, 60626 </h3>
+				</div>
+				<div className={cx('form')}>
+					<form>
+						<div className={cx('first-row-inputs')}>
+							<input className={cx('first')} type='text' placeholder='your name' />
+							<input type='text' placeholder='your email' />
 						</div>
-					</Col>
-				</Row>
-      </Grid>
+						<input type='textarea' placeholder="how'd you hear about this event? (optional)" />
+						<div className={cx('btns')}>
+							<div className={cx('submit')}>
+								<button> SUBMIT </button>
+							</div>
+							<div className={cx('submit')}>
+								<button> ENTER SITE </button>
+							</div>
+						</div>
+					</form>
+				</div>
+				<video autoPlay muted loop className={cx('bkg')}>
+				  <source src="/video/eyes-closed.mp4" type="video/mp4" />
+				</video>
+      </div>
     );
   }
 }
