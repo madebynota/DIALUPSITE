a5d7ed36658b94528997269a6d2244c6d3f98221
David Latimore II <david.latimore@me.com>
2019-11-07 02:15:29 -0500

    added in the rsvp form and connected to firebase, pushing to staging

--- src/components/SplashPage/SplashPage.jsx
+++ src/components/SplashPage/SplashPage.jsx
@@ -2,19 +2,96 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import classNames from 'classnames/bind';
 import Utils from '../../utils.js';
+import firebase from '../../firebase.js';
 import styles from './styles/SplashPage.css';
 
 let cx = classNames.bind(styles);
 
 class SplashPage extends React.Component {
 	constructor(props) {
-    super(props);
-  }
+		super(props);
+		this.state = {
+			nameError: false,
+			emailError: false,
+			name: '',
+			email: '',
+			referral: '',
+			submitted: false,
+		}
+		this.handleNameChange = this.handleNameChange.bind(this);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.handleReferralChange = this.handleReferralChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  render() {
-    return (
-      <div className={cx('RSVPPage')}>
-        <img src='/img/graphics/title.svg' className={cx('title')}/>
+	handleNameChange(e) {
+		this.setState({
+			name: e.target.value
+		})
+	}
+
+	handleEmailChange(e) {
+		this.setState({
+			email: e.target.value
+		})
+	}
+
+	handleReferralChange(e) {
+		this.setState({
+			referral: e.target.value
+		})
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const {name, email, referral} = this.state;
+		if (name.length <= 1) {
+			this.setState({
+				nameError: true
+			});
+		} else if (email.length < 4) {
+			this.setState({
+				emailError: true,
+			});
+		} else {
+			const guestsRef = firebase.database().ref('guests');
+			const guestInfo = {
+				name: this.state.name,
+				email: this.state.email,
+				referral: this.state.referral,
+			}
+			guestsRef.push(guestInfo);
+			this.setState({
+				emailError: false,
+				nameError: false,
+				submitted: true,
+				name: '',
+				email: '',
+				referral: ''
+			});
+		}
+	}
+
+	render() {
+		const { nameError, emailError, submitted } = this.state;
+		const errorStyles = {
+			display: !nameError && !emailError ? 'none' : 'flex',
+			color: 'red',
+			marginTop: '10px'
+		}
+
+		const textareaStyles = submitted ? {
+			marginBottom: '40px',
+		} : null;
+
+		const submittedStyles = {
+			display: submitted ? 'flex' : 'none',
+			marginBottom: '30px'
+		};
+
+		return (
+			<div className={cx('RSVPPage')}>
+				<img src='/img/graphics/title.svg' className={cx('title')}/>
 				<div className={cx('subtitles')}>
 					<h2 className={cx('subtitle')}> release party & live performance </h2>
 					<h2 className={cx('subtitle', 'bottom')}> release party & live performance </h2>
@@ -26,14 +103,20 @@ class SplashPage extends React.Component {
 				<div className={cx('form')}>
 					<form>
 						<div className={cx('first-row-inputs')}>
-							<input className={cx('first')} type='text' placeholder='your name' />
-							<input type='text' placeholder='your email' />
+							<input onChange={this.handleNameChange} className={cx('first')} type='text' placeholder='your name' value={this.state.name} />
+							<input onChange={this.handleEmailChange} type='text' placeholder='your email' value={this.state.email} />
 						</div>
-						<input type='textarea' placeholder="how'd you hear about this event? (optional)" />
+						<h6 style={errorStyles} className={cx('error')}> please enter a valid email / name </h6>
+						<input style={textareaStyles} onChange={this.handleReferralChange} type='textarea' placeholder="how'd you hear about this event? (optional)" value={this.state.referral} />
+						{submitted && (
+							<h6 style={submittedStyles} className={cx('error')}> response submitted. see you next week! </h6>
+						)}
 						<div className={cx('btns')}>
-							<div className={cx('submit')}>
-								<button> SUBMIT </button>
-							</div>
+							{!submitted && (
+								<div className={cx('submit')}>
+									<button onClick={this.handleSubmit}> SUBMIT </button>
+								</div>
+							)}
 							<div className={cx('submit')}>
 								<button> ENTER SITE </button>
 							</div>
@@ -41,11 +124,11 @@ class SplashPage extends React.Component {
 					</form>
 				</div>
 				<video autoPlay muted loop className={cx('bkg')}>
-				  <source src="/video/eyes-closed.mp4" type="video/mp4" />
+					<source src="/video/eyes-closed.mp4" type="video/mp4" />
 				</video>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default SplashPage;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+const config = {
+  apiKey: "AIzaSyBVhZ5SAhioqIebmumLMpNzr58JSOZ-a4I",
+  authDomain: "eyes-closed-ef883.firebaseapp.com",
+  databaseURL: "https://eyes-closed-ef883.firebaseio.com",
+  projectId: "eyes-closed-ef883",
+  storageBucket: "eyes-closed-ef883.appspot.com",
+  messagingSenderId: "1051050261299",
+  appId: "1:1051050261299:web:5e6467ba7789ec7179dc46",
+  measurementId: "G-670QJM7NGF"
+};
+firebase.initializeApp(config);
+export default firebase;
