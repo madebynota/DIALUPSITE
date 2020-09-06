339b7f43b96b5c5f9d9e38879bff2e127149f27e
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-09-06 15:04:09 -0400

    Get boface animation running

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,33 +1,33 @@
-import browser from 'browser-detect'
-import { setupAndrewFace, drawAndrewFace } from "./andrewFaceAnimation";
-import { setupBoFace, drawBoFace } from "./boFaceAnimation";
+// import browser from 'browser-detect'
+// import { setupAndrewFace, drawAndrewFace } from "./andrewFaceAnimation";
+// import { setupBoFace, drawBoFace } from "./boFaceAnimation";
 
-const result = browser();
-const drawOnPath = '/home';
+// const result = browser();
+// const drawOnPath = '/home';
 
-export function setup() {
-    if (window.location.pathname != drawOnPath) {
-        return;
-    }
+// export function setup() {
+//     if (window.location.pathname != drawOnPath) {
+//         return;
+//     }
 
-    if (browser.name == "safari") {
-        setupAndrewFace();
-    }
-    else {
-        setupBoFace();
-    }
-}
+//     if (browser.name == "safari") {
+//         setupAndrewFace();
+//     }
+//     else {
+//         setupBoFace();
+//     }
+// }
 
-export function draw() {
-    if (window.location.pathname != drawOnPath) {
-        return;
-    }
+// export function draw() {
+//     if (window.location.pathname != drawOnPath) {
+//         return;
+//     }
 
-    if (browser.name == "safari") {
-        drawAndrewFace();
-    }
-    else {
-        drawBoFace();
-    }
+//     if (browser.name == "safari") {
+//         drawAndrewFace();
+//     }
+//     else {
+//         drawBoFace();
+//     }
 
-}
+// }
