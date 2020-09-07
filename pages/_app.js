308a0f63b706c3f3ff1670d160d8606b18f6029f
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-09-07 09:23:56 -0400

    Fix html attribute names

--- pages/_app.js
+++ pages/_app.js
@@ -65,9 +65,9 @@ function MyApp({ Component, pageProps }) {
         <!-- MMMMMMMMMMMMMMMMMMMMM~IMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM -->
 
         <!-- Metadata --> */}
-        <meta charset="utf=8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=Edge;chrome=1" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charSet="utf=8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=Edge;chrome=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
         {/* <!--Favicon--> */}
