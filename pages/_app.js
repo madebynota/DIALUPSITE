270ed3d130a860ed5a9a79d66e61b2c341cae450
David Latimore II <david.latimore@me.com>
2021-04-21 09:30:28 -0500

    updating open graph, favicon

--- pages/_app.js
+++ pages/_app.js
@@ -71,22 +71,22 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
         {/* <!--Favicon--> */}
-        <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png"/>
-        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png"/>
-        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png"/>
-        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png"/>
-        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png"/>
-        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png"/>
-        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png"/>
-        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png"/>
-        <link rel="icon" type="image/png" sizes="192x192"  href="img/favicon/android-icon-192x192.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png"/>
-        <link rel="manifest" href="img/favicon/manifest.json"/>
+        <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png"/>
+        <link rel="manifest" href="manifest.json"/>
         <meta name="msapplication-TileColor" content="#ffffff"/>
-        <meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png"/>
+        <meta name="msapplication-TileImage" content="img/ms-icon-144x144.png"/>
         <meta name="theme-color" content="#ffffff"/>
 
         {/* <!--GSuite Verification--> */}
@@ -106,7 +106,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:creator" content="@DialUpStuff"/>
         <meta name="twitter:title" content="DIAL UP"/>
         <meta name="twitter:description" content="Dial Up"/>
-        <meta name="twitter:image:src" content="http://dialupstuff.com/img/favicon/embedImage.png"/>
+        <meta name="twitter:image:src" content="http://dialupstuff.com/img/open-graph.png"/>
         {/* <!-- at least 280px x 150px for Twitter (image must be < 1MB) --> */}
         <meta name="twitter:domain" content="dialupstuff.com"/>
 
